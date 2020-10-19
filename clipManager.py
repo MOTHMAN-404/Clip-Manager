783ca392e6deed8db32a9db4a8eb723dceebe5f2
MOTHMAN-404 <shawnmcdermott17@gmail.com>
2020-10-19 17:48:55 -0400

    Added comments

--- clipManager.py
+++ clipManager.py
@@ -11,20 +11,24 @@
 '''
 import tkinter, pyperclip, keyboard
 
+# When a copy hotkey is pressed
 def copy_triggered():
         print('copy detected')
 
+# When a paste hotkey is pressed
 def paste_triggered():
         print('paste detected')
 
+# Main function
 def main():
-        for i in range(10):
-                copyCombo = 'z +' + str(i)
-                pasteCombo = 'x +' + str(i)
-                keyboard.add_hotkey(copyCombo, copy_triggered)
-                keyboard.add_hotkey(pasteCombo, paste_triggered)
-
+        for i in range(10):            
+                copyCombo = 'z +' + str(i)     # Copy hotkeys 0-9
+                pasteCombo = 'x +' + str(i)    # Paste hotkeys 0-9
+                keyboard.add_hotkey(copyCombo, copy_triggered) # When combo is detected
+                keyboard.add_hotkey(pasteCombo, paste_triggered) # Call the triggered functions
+        
         keyboard.wait() 
 
+# Calls main function when the program starts
 if __name__ == "__main__":
         main()
